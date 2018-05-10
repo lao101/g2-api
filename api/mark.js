b88ab69956368878d09512e3aa7e3f0530b865cf
Surachai Visesvoharn <lao101@gmail.com>
2018-05-10 10:32:22 +0700

    อ.สมศักดิ์ ปรับแก้การเพิ่มคะแนน

--- api/mark.js
+++ api/mark.js
@@ -12,6 +12,21 @@ router.get('/', async (req, res)=>{
     })
 })
 
-router.post('/add', async (req, res)=>{
+router.post('/save', async (req, res)=>{
     let db = req.db
+    // insert into mark (teachid, sbjid, sid, mark) values(1,1,1, 5), (1,1,2, 6)
+    // let sql = 'insert into mark (teachid, sbjid, sid, mark) values '
+    //     + req.body.marks.map(x => '(' + req.body.teacherId + ', ' + req.body.subid + ', ' + x.sid + ', ' + x.mark + ')').join(', ')
+    try {
+        let ids = await db('marks').insert(req.body.marks.map(x => ({
+            mark: x.mark,
+            grade: 3,
+            student_id: x.sid,
+            teacher_id: req.body.teacherId,
+            subjCode: req.body.subid,
+        })))
+        res.send({ok: true})
+    } catch (e) {
+        res.send({ok: false, e: e.message})
+    }
 })

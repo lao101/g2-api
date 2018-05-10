befca4d80c7ab1f4d3cb8fd0ab4b20ad349dae45
Surachai Visesvoharn <lao101@gmail.com>
2018-05-10 11:15:49 +0700

    update save to db

--- api/mark.js
+++ api/mark.js
@@ -17,10 +17,11 @@ router.post('/save', async (req, res)=>{
     // insert into mark (teachid, sbjid, sid, mark) values(1,1,1, 5), (1,1,2, 6)
     // let sql = 'insert into mark (teachid, sbjid, sid, mark) values '
     //     + req.body.marks.map(x => '(' + req.body.teacherId + ', ' + req.body.subid + ', ' + x.sid + ', ' + x.mark + ')').join(', ')
+
     try {
         let ids = await db('marks').insert(req.body.marks.map(x => ({
             mark: x.mark,
-            grade: 3,
+            grade: x.grade,
             student_id: x.sid,
             teacher_id: req.body.teacherId,
             subjCode: req.body.subid,
